cd8ff0f38643d0e6a4daeae01036054902c37c30
Michael Feig <mfeiglab@gmail.com>
2025-11-02 08:39:08 -0500

    cleaned up code

--- src/cocomo/__init__.py
+++ src/cocomo/__init__.py
@@ -1,25 +1,19 @@
 from .__version__ import __version__
-from .cocomocg import COCOMO
-from .structure import (
-    Atom,
-    Chain,
+from .cocomo_system import COCOMO
+from .molecule_handling import (
     DomainSelector,
     Model,
     PDBReader,
-    Residue,
     SelectionError,
     Structure,
 )
 
 __all__ = [
     "__version__",
-    "Atom",
-    "Chain",
     "COCOMO",
     "DomainSelector",
     "Model",
     "PDBReader",
-    "Residue",
     "SelectionError",
     "Structure",
 ]
